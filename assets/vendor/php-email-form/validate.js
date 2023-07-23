d3ac33c63cab3ea737dcfc356cee78df3d3f4533
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2023-07-23 16:06:41 +0530

    url change

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,6 +1,6 @@
 /**
 * PHP Email Form Validation - v3.6
-* URL: https://bootstrapmade.com/php-email-form/
+* URL: https://suvradeep.vercel.app/php-email-form/
 * Author: BootstrapMade.com
 */
 (function () {
